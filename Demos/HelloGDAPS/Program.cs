c41daf653813b1f5cfa6f134fdad9781555e4a6e
Erika Mesh <1838833+esmesh@users.noreply.github.com>
2026-08-29 21:11:41 -0400

    Initial commit

---
+++ Demos/HelloGDAPS/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Demo project created by Prof. Mesh so that
+* everyone would have something in their new
+* GitHub repo from the start. :)
+*/
+
+namespace HelloGDAPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print some text to the console to be sure everything works
+            Console.WriteLine("Hello GDAPS student!");
+            Console.WriteLine("You successfully cloned and ran a project from GitHub!");
+        }
+    }
+}
