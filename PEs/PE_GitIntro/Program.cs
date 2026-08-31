0fad0c1818283f49125bd947abc46730641c6e91
Erika Mesh <esmvcs@rit.edu>
2026-08-31 10:19:01 -0400

    Created a new PE in git!

---
+++ PEs/PE_GitIntro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE_GitIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
