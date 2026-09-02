ea6270736d296d773d351961c2aed32379108e23
Erika Mesh <esmvcs@rit.edu>
2026-09-02 10:24:31 -0400

    W 9/2 - Data types & Variables

---
+++ Demos/Week 2/DataAndVars/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DataAndVars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int num = 10.75; // won't compile
+            double bigNum = 10;
+
+            const int MaxHealth = 100;
+
+            string shiro;
+            int x = 3;
+            int y = 4;
+            int z;
+
+            //MaxHealth = 10;
+
+            shiro = "my cat too?";
+            Console.WriteLine(shiro);
+            Console.WriteLine(x + y);
+            z = x + y;
+            Console.WriteLine(z);
+
+            //            z = z + 1;
+            // z += 1;
+            z++;
+            Console.WriteLine(z);
+            Console.WriteLine(MaxHealth);
+
+            // shiro = z; // BAD
+        }
+    }
+}
