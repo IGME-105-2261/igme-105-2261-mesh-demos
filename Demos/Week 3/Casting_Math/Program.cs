c7775fc497ddb55fb301fd87522f6a8028e16027
Erika Mesh <esmvcs@rit.edu>
2026-09-09 09:52:15 -0400

    Wed 9/9 - Casting and the Math class

---
+++ Demos/Week 3/Casting_Math/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Casting_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable type & data match
+            int dollars = 45;
+            int cents = 33;
+
+            // implicit casting
+            double price = 10; // 10 without .0 -- int "literal"
+            price = dollars;
+            price += cents / 100.0;
+
+            // explicit
+            dollars = (int)price; // force copying ONLY the int portion
+
+
+            price += 1 / 3.0;
+            Console.WriteLine(Math.Round(price, 2));
+
+        }
+    }
+}
