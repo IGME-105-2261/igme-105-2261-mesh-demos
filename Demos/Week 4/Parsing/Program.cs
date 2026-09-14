66dfc5de3a5b7583123cff443a2129ccc0101d49
Erika Mesh <esmvcs@rit.edu>
2026-09-14 10:48:33 -0400

    M 9/14 - Parsing

---
+++ Demos/Week 4/Parsing/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // What type of cast does this need?
+            // int a = (int)10.5; // explicit
+
+            // "12"
+            // '1' '2'
+            // 1*10 + 2*1
+            // 12
+
+            int num;
+            //string input;
+            Console.Write("Enter a whole number: ");
+            //input = Console.ReadLine();
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine(num * 10);
+
+        }
+    }
+}
